04a4fcca71e9480fb4ae3d95cac0e341ff121665
임수영 <christine8739@duksung.ac.kr>
2026-03-11 22:38:18 +0900

    3월 10일 수업 종료(01반)

---
+++ DP_2026-1/src/practice/ch01/main.java
@@ -0,0 +1,8 @@
+pacakge practice.ch01;
+
+public class main{
+    public static void main(String[] args) {
+        Book book = new Book("Java Programming");
+        System.out.println("Book name: " + book.getName());
+    }
+}
