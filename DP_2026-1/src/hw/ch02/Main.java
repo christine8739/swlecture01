f79cdb0d2ce2f47fc210183b069bade75947c8ec
임수영 <christine8739@duksung.ac.kr>
2026-03-23 08:12:55 +0900

    과제 2 수정 완료

--- DP_2026-1/src/hw/ch02/Main.java
+++ DP_2026-1/src/hw/ch02/Main.java
@@ -3,6 +3,8 @@ package hw.ch02;
 public class Main {
     //한국 콘선트 Adpater로 충전
     public static void main(String[] args) {
+        //무조건 앞에 내 학번과 이름 넣기
+        System.out.println("학번: 20240886 이름: 임수영");
          //KoreanOutletAdapter 생성
         KoreanOutlet koreanOutlet = new KoreanOutlet();
         //KoreanOutletAdapter 생성-provide()를 charge()로 변환
